a967cb1a43037d4d74926d7196ae11ca9d43ea53
Elik <zamitov123@gmail.com>
2026-06-02 12:11:50 +0500

    fix

--- magazin/src/main.tsx
+++
@@ -1,9 +0,0 @@
-
-import { createRoot } from 'react-dom/client'
-import './i18n/config'  
-import './styles/global.scss'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <App />
-)
